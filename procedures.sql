c3dfa29fa51ad912007b0d6ba9d0db8b8975e476
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-03-13 11:34:24 -0300

    prcodeures

--- procedures.sql
+++ procedures.sql
@@ -1,3 +1,5 @@
+/*Create*/
+
 CREATE PROCEDURE Inclui_Cliente
 	@RG VARCHAR(9),
     @NOME VARCHAR(50), 
@@ -14,9 +16,10 @@ BEGIN
     INSERT INTO CLIENTES ([RG],[NOME], [ENDERECO], [BAIRRO], [CIDADE], [ESTADO], [TELEFONE], [EMAIL], [DATANASCIMENTO], [Sexo]) 
     VALUES (@RG,@NOME, @ENDERECO, @BAIRRO, @CIDADE, @ESTADO, @TELEFONE, @EMAIL, @DATANASCIMENTO, @Sexo)
 END 
+/*Feita em casa e na escola*/
 EXEC Inclui_Cliente '123456789', 'João Silva', 'Rua das Flores, 123', 'Centro', 'São Paulo', 'SP', '11999999999', 'joao@email.com', '1990-01-01', 'M';
 
-/*Feita em casa e na escola*/
+
 /*Vai aparecer erro  no create procedure mas isso acontece pq tem mais de um create no mesmo documento
 Vai rodar normal desde q vc n rode os dois ao mesmo tempo, acho*/
 CREATE PROCEDURE Inclui_Categoria
@@ -26,6 +29,66 @@ BEGIN
     INSERT INTO CATEGORIA([NOME_CATEGORIA]) 
     VALUES (@NOME_CATEGORIA)
 END
-/*criada em casa*/
+/*criada em casa e na escola*/
 EXEC Inclui_Categoria 'Sci-fi';
 select * from CATEGORIA;
+
+/* READ*/
+CREATE PROCEDURE Mostra_Clientes
+AS
+BEGIN 
+    select * from CLIENTES;
+END
+/*criada na escola*/
+exec Mostra_Clientes;
+
+CREATE PROCEDURE Mostra_Cliente_Por_ID
+	@ID numeric(18,0)
+AS
+BEGIN 
+    select * from CLIENTES where COD_CLIENTE = @ID
+END
+exec Mostra_Cliente_Por_ID 5;
+/*criada na escola*/
+/*Update*/
+CREATE PROCEDURE Update_Dados_Cliente
+	@COD_CLIENTE numeric(18,0),
+	@RG VARCHAR(9),
+    @NOME VARCHAR(50), 
+    @ENDERECO VARCHAR(50), 
+    @BAIRRO VARCHAR(30), 
+    @CIDADE VARCHAR(30), 
+    @ESTADO CHAR(2),
+    @TELEFONE VARCHAR(15),
+    @EMAIL VARCHAR(30), 
+    @DATANASCIMENTO DATETIME, 
+    @Sexo CHAR(1)
+AS
+BEGIN 
+    UPDATE CLIENTES
+    SET
+        RG = @RG,
+        NOME = @NOME,
+        ENDERECO = @ENDERECO,
+        BAIRRO = @BAIRRO,
+        CIDADE = @CIDADE,
+        ESTADO = @ESTADO,
+        TELEFONE = @TELEFONE,
+        EMAIL = @EMAIL,
+        DATANASCIMENTO = @DATANASCIMENTO,
+        Sexo = @Sexo
+    WHERE COD_CLIENTE = @COD_CLIENTE
+END
+/*criada na escola*/
+EXEC Update_Dados_Cliente 12, '123456789', 'João Silva', 'Rua das Flores, 123', 'Centro', 'São Paulo', 'SP', '11999999999', 'joaosilva@email.com', '1990-01-01', 'M';
+
+/*Delete*/
+
+
+CREATE PROCEDURE Deleta_cliente
+	@ID numeric(18,0)
+AS
+BEGIN 
+    delete from CLIENTES where COD_CLIENTE = @ID
+END
+select count(*) from CLIENTES;
