48167d78b17babefb921903d7fd4b3878eab79f8
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-03-13 14:44:28 -0300

    1 e 2 atividade

---
+++ atividade.sql
@@ -0,0 +1,78 @@
+/*Create*/
+CREATE PROCEDURE Inclui_Cliente
+	@RG VARCHAR(9),
+    @NOME VARCHAR(50), 
+    @ENDERECO VARCHAR(50), 
+    @BAIRRO VARCHAR(30), 
+    @CIDADE VARCHAR(30), 
+    @ESTADO CHAR(2),
+    @TELEFONE VARCHAR(15),
+    @EMAIL VARCHAR(30), 
+    @DATANASCIMENTO DATETIME, 
+    @Sexo CHAR(1)
+AS
+BEGIN 
+    INSERT INTO CLIENTES ([RG],[NOME], [ENDERECO], [BAIRRO], [CIDADE], [ESTADO], [TELEFONE], [EMAIL], [DATANASCIMENTO], [Sexo]) 
+    VALUES (@RG,@NOME, @ENDERECO, @BAIRRO, @CIDADE, @ESTADO, @TELEFONE, @EMAIL, @DATANASCIMENTO, @Sexo)
+END 
+/*Read*/
+CREATE PROCEDURE Mostra_Clientes
+AS
+BEGIN 
+    select * from CLIENTES;
+END
+/*criada na escola*/
+
+
+CREATE PROCEDURE Mostra_Cliente_Por_ID
+	@ID numeric(18,0)
+AS
+BEGIN 
+    select * from CLIENTES where COD_CLIENTE = @ID
+END
+/*Update*/
+CREATE PROCEDURE Update_Dados_Cliente
+	@COD_CLIENTE numeric(18,0),
+	@RG VARCHAR(9),
+    @NOME VARCHAR(50), 
+    @ENDERECO VARCHAR(50), 
+    @BAIRRO VARCHAR(30), 
+    @CIDADE VARCHAR(30), 
+    @ESTADO CHAR(2),
+    @TELEFONE VARCHAR(15),
+    @EMAIL VARCHAR(30), 
+    @DATANASCIMENTO DATETIME, 
+    @Sexo CHAR(1)
+AS
+BEGIN 
+    UPDATE CLIENTES
+    SET
+        RG = @RG,
+        NOME = @NOME,
+        ENDERECO = @ENDERECO,
+        BAIRRO = @BAIRRO,
+        CIDADE = @CIDADE,
+        ESTADO = @ESTADO,
+        TELEFONE = @TELEFONE,
+        EMAIL = @EMAIL,
+        DATANASCIMENTO = @DATANASCIMENTO,
+        Sexo = @Sexo
+    WHERE COD_CLIENTE = @COD_CLIENTE
+END
+/*Delete*/
+CREATE PROCEDURE Deleta_cliente
+	@ID numeric(18,0)
+AS
+BEGIN 
+    delete from CLIENTES where COD_CLIENTE = @ID
+END
+select count(*) from CLIENTES;
+
+/*2)*/
+CREATE PROCEDURE MostraClientesAniversarioPorMes
+    @mes numeric(2,0)
+AS
+BEGIN 
+    select Nome, Day(DATANASCIMENTO) from CLIENTES where Month(DATANASCIMENTO) = @mes
+END
+EXEC MostraClientesAniversarioPorMes 5;
