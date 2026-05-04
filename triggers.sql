41b4ca6bf898a288bf725f0d79754c6245e86ffe
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-05-04 12:25:52 -0300

    lojinha resenha

--- triggers.sql
+++ triggers.sql
@@ -94,7 +94,7 @@ end
 
 
 
-drop trigger devolve_filme
+
 create trigger Devolve_Filme on LOCACOES
 after update 
 as 
@@ -104,7 +104,42 @@ end
 
 
 /*Lojinha*/
-
 create table lj_produto (
-	
+	id int identity(1,1) primary key not null ,
+	descricao varchar(250),
+	unidade varchar(10),
+	preco int not null
 );
+alter table lj_produto
+add nome varchar(50);
+alter table lj_produto
+alter column preco float;
+create table lj_venda (
+	id int identity(1,1) primary key not null,
+	id_prod int FOREIGN KEY REFERENCES lj_produto(id) not null, 
+	dia datetime not null, 
+	id_cliente int not null, 
+	qtd int not null
+);
+create table lj_compra (
+	id int identity(1,1) primary key not null,
+	id_prod int FOREIGN KEY REFERENCES lj_produto(id) not null, 
+	dia datetime not null, 
+	id_fornecedor int not null, 
+	qtd int not null
+);
+
+create table lj_estoque (
+	id int identity(1,1) primary key, 
+	id_prod int FOREIGN KEY REFERENCES lj_produto(id) not null , 
+	qtd int
+);
+drop trigger AdProdEstoque
+create trigger AdProdEstoque on lj_produto
+after insert 
+as
+begin
+	insert into lj_estoque(id_prod,qtd) values ((select id from inserted),0)
+end
+
+insert into lj_produto(nome, unidade, preco, descricao) values ('Monster', 'Lata', 11.9, 'Monster original');
